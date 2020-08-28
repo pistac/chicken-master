78b1931a1c33c36d3e6ee0b22353b45d8d38d1a3
pistac <ilaria.torre@cognovo.eu>
2020-08-28 18:27:36 +0200

    fix typo

--- Chicken/Assets/Scripts/ExperimentDataManager.cs
+++ Chicken/Assets/Scripts/ExperimentDataManager.cs
@@ -206,11 +206,11 @@ public class ExperimentDataManager : MonoBehaviour {
       // we are not allowed to fetch data from the url, but the data is still posted correctly.
       if (w.isNetworkError || w.isHttpError) {
         Debug.LogError(w.error);
-		else
-        {
-            Debug.Log("Form upload complete!");
-        }
       }
+	  else
+	  {
+		  Debug.Log("Form upload complete!");
+		}
 
       dataSentText.SetActive(true);
     }
